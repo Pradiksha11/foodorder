35d5aa32e0774cbce3f9fe0f5ac5e79ff19c683d
Sourabh <kanadesourabh420@gmail.com>
2021-12-17 19:55:10 +0530

    all queries updated

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import meal from '../../assets/meals.jpg';
+// import meal from '../../assets/meals.jpg';
 import style from './Header.module.css';
 import HeaderCartButton from '../layout/HeaderCartButton';
 import logo from '../../assets/Fish420.png';
@@ -14,9 +14,9 @@ const Header = (props) => {
       </a>
        <HeaderCartButton onClick={props.onOpen} />   {/* here props.onOpen value has came from App.js line-22 and again passed to lowest compon. HeaderCartButton line-15 */}
         </header>
-      <div className={style['main-image']}>
+      {/* <div className={style['main-image']}>
       <img src={meal} alt="meal"/>
-      </div>
+      </div> */}
     </>
   )
 };
